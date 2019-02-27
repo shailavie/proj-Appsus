e48617d6d300ba284757cd1babe1c69ca23bde8f
Sarel <Sarelk@gmail.com>
2019-02-27 15:49:05 +0200

    layout just for work emails

--- js/apps/email/pages/email-app-cmp.js
+++ js/apps/email/pages/email-app-cmp.js
@@ -1,7 +1,35 @@
+import emailService from '../services/email-service.js';
+
 export default {
-    template:`
-    <section>
-        <h1> I am the Email app</h1>
-    </section>
-    `
+    template: `
+        <section class="e-app">
+            <div class="e-sidebar">
+                <button>Compose</button><br/>
+                Inbox <br/>
+                Starred <br/>
+                Sent Mail<br/>
+                Drafts<br/>
+                Progress Bar
+            </div>
+
+            <div class="e-main-emails">
+                emails
+            </div>
+        </section> 
+    `,
+    data() {
+        return {
+        }
+    },
+    created() {
+        window.document.title='Email App'
+    },
+    methods: {
+    },
+    computed: {
+        
+    },
+    components: {
+
+    }
 }
---
+++ js/apps/email/services/email-service.js
@@ -0,0 +1,5 @@
+import utilService from '../services/util-service.js';
+
+export default {
+
+}
