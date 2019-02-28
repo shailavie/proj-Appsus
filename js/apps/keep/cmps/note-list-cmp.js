075f3b8bd09e4b362643865fdadcef3048f75c81
Shai Lavie <shai.lavie@gmail.com>
2019-02-28 17:51:10 +0200

    added sections for pinned and others

--- js/apps/keep/cmps/note-list-cmp.js
+++ js/apps/keep/cmps/note-list-cmp.js
@@ -9,10 +9,10 @@ export default {
            :cols="{default: 4, 1000: 3, 700: 2, 500: 1}" 
            :gutter="{default: '20px', 700: '20px'}"
            >
-                <div 
+           <transition-group tag="div" class="note-list" name="fade"
                     :to="'/note/' + currNote.id" 
                     v-for="(currNote, idx) in notes" 
-                    :key="currNote.id" 
+               
                     :note="currNote">
                     
                     <note-preview 
@@ -20,9 +20,10 @@ export default {
                         :note="currNote" 
                         :search="search"
                         :idx="idx+1" 
+                        :key="currNote.id" 
                         @click.native="selectNote(currNote)">
                     </note-preview>
-                </div>
+            </transition-group>
             </masonry>
     `,
     props: ['notes','search'],
@@ -39,3 +40,48 @@ export default {
         notePreview
     }
 }
+
+
+
+// TO DO - MAKE THE LIST WORKS WITH DYNAMIC COMPONENTS
+
+// import notePreview from './note-preview-cmp.js';
+
+// export default {
+//     template: `
+//            <masonry 
+//            :cols="{default: 4, 1000: 3, 700: 2, 500: 1}" 
+//            :gutter="{default: '20px', 700: '20px'}"
+//            >
+//            <transition-group tag='div' class='note-list'>
+//                 <div 
+//                     :to="'/note/' + currNote.id" 
+//                     v-for="(currNote, idx) in notes" 
+//                     :key="currNote.id" 
+//                     :note="currNote">
+                    
+//                     <note-preview 
+//                         class="masonry-brick" 
+//                         :note="currNote" 
+//                         :search="search"
+//                         :idx="idx+1" 
+//                         @click.native="selectNote(currNote)">
+//                     </note-preview>
+//                 </div>
+//             </transition-group>
+//             </masonry>
+//     `,
+//     props: ['notes','search'],
+//     methods: {
+//         selectNote(note) {
+//             console.log(note,'is selected!')
+//             this.$emit('selected', note)
+//         }
+//     },
+//     created() {
+//         console.log('got :',this.search)
+//     },
+//     components: {
+//         notePreview
+//     }
+// }
