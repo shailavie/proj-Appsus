9a99efe8f1ad4028a1f5626f8b213f432da6cae1
Shai Lavie <shai.lavie@gmail.com>
2019-03-03 22:01:44 +0200

    home cmp

--- js/home-cmp.js
+++ js/home-cmp.js
@@ -1,7 +1,32 @@
 import { eventBus, EVENT_NOTE_TO_EMAIL } from './services/eventbus-service.js'
 
 export default  {
-    template: `<h1> Welcome to Appsus. Choose youre App</h1>`
+    template: `
+        <section>
+            <div class="home-apps">
+                <div class="apps-container">
+                    <div class="app-container">
+                        <router-link to="/email"  class="nb-link" title="Email">
+                            <i class="fas fa-envelope-open"></i>
+                            <h2>Email</h2>
+                        </router-link>
+                    </div>
+                    <div class="app-container">
+                        <router-link to="/keep"  class="nb-link" title="Keep">
+                            <i class="fas fa-sticky-note"></i>
+                            <h2>Keep</h2>
+                        </router-link>
+                    </div>
+                    <div class="app-container">
+                        <router-link to="/books" class="nb-link" title="Books">
+                            <i class="fas fa-book"></i>
+                            <h2>Books</h2>
+                        </router-link>
+                    </div>
+                </div>
+            </div>
+        </section>
+        `
     ,created(){
         console.log('home is adding event BUS')
         eventBus.$on(EVENT_NOTE_TO_EMAIL, emailFromNote => {
